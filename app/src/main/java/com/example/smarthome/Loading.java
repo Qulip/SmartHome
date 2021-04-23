72373c429916a2f0f3fb197fa8658fd426cd3c59
Qulip <ilkwon.ryu@gmail.com>
2021-04-24 04:00:49 +0900

    First push

---
+++ app/src/main/java/com/example/smarthome/Loading.java
@@ -0,0 +1,32 @@
+package com.example.smarthome;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_loading);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 4000);
+    }
+}
---
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.smarthome;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, Loading.class);
+        startActivity(intent);
+
+    }
+}
