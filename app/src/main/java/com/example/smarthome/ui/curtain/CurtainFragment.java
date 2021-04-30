1a171329d3bc306dc7a9f38d5739f86009630cbf
Qulip <ilkwon.ryu@gmail.com>
2021-05-01 06:02:21 +0900

    change CurtainFragment and add some images

--- app/src/main/java/com/example/smarthome/ui/curtain/CurtainFragment.java
+++ app/src/main/java/com/example/smarthome/ui/curtain/CurtainFragment.java
@@ -1,13 +1,17 @@
 package com.example.smarthome.ui.curtain;
 
+import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -17,19 +21,24 @@ import com.example.smarthome.R;
 public class CurtainFragment extends Fragment {
 
     private CurtainViewModel curtainViewModel;
+    ImageView white_rect;
+    GradientDrawable drawable;
+    ImageView context;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         curtainViewModel =
                 new ViewModelProvider(this).get(CurtainViewModel.class);
         View root = inflater.inflate(R.layout.fragment_curtain, container, false);
-        final TextView textView = root.findViewById(R.id.text_curtain);
-        curtainViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        white_rect = (ImageView) white_rect.findViewById(R.id.cur_whiterect);
+
+        GradientDrawable drawable = (GradientDrawable) context.getDrawable(R.drawable.background_rounding);
+
+        white_rect.setBackground(drawable);
+        white_rect.setClipToOutline(true);
+
         return root;
     }
 }
